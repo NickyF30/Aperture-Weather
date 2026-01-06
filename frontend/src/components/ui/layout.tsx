0a82fa1bffc511027b230d826490bcf15047f110
Nicky Fang <nfang3885@gmail.com>
2026-01-06 15:39:16 -0500

    Fixed issues with rendering by ensuring react-router-dom was installed in frontend

--- frontend/src/components/ui/layout.tsx
+++ frontend/src/components/ui/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
@@ -5,15 +6,18 @@ const Layout = ({ children }: PropsWithChildren) => {
         
         <div className="bg-gradient-to-br from-background to-muted">
             {/* shadUI defined background color to muted color, handles light and dark mode automatically */}
-            
+
             header
 
             {/* shadUI container class for responsive padding and centering */}
-            <main className="min-h-screen container mx-auto px-4 py-8">
-                
+            <main className="min-h-screen container mx-auto px-4 py-8">    
             {children}
             </main>
-            footer  
+            <footer className ="border-t backdrop-blur py-12">
+                <div className ="container mx-auto px-4 text-center text-gray-600">
+                    <p> Hello testing </p>
+                </div>
+            </footer>
 
         </div>
     );
