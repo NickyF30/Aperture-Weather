e1222b50487b9f3187bacf8afcac532534b1dd38
Nicky Fang <nfang3885@gmail.com>
2026-01-07 20:49:15 -0500

    Initialized dotenv and added city based routing in index.ts. Additionally added a check for api key

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
 import Fastify from 'fastify'
 import { config } from 'dotenv';
 import { WeatherService } from './services/weather.service';
+import cors from '@fastify/cors';
+
+config();
 
 const app = Fastify({ logger: true });
+
+// check for API key
+if (!process.env.OPENWEATHER_API_KEY) {
+    app.log.error('Missing OPENWEATHER_API_KEY in environment');
+    process.exit(1);
+}
+
 const weatherService = new WeatherService(process.env.OPENWEATHER_API_KEY!);
 
 // Enable CORS for frontend
@@ -14,6 +24,7 @@ app.get('/health', async () => {
   return { status: 'ok' };
 });
 
+//weather by lat lon route for frontend
 app.get('/api/weather', async (request, reply) => {
   try {
     const { lat, lon } = weatherService.validateQuery(request.query);
@@ -23,6 +34,18 @@ app.get('/api/weather', async (request, reply) => {
     app.log.error(error);
     reply.code(400).send({ error: 'Invalid request' });
   }
+});
+
+//city based route for frontend
+app.get('/api/weather/city', async (request, reply) => {
+    try {
+      const { q } = weatherService.validateCityQuery(request.query);
+      const { lat, lon } = await weatherService.getCoordinatesByCity(q);
+      const weather = await weatherService.getCurrentWeather(lat, lon);
+      return weather;
+    } catch (error: any) {
+      return reply.code(404).send({ error: error.message || 'City search failed' });
+    }
 });
 
 app.listen({ port: Number(process.env.PORT) || 3001 }, (err) => {
