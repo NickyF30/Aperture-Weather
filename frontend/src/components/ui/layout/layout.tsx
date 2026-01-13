5142c89db47f2925aee0356e98154de8edfde39b
Nicky Fang <nfang3885@gmail.com>
2026-01-12 19:47:10 -0500

    Removed testing message at footer

--- frontend/src/components/ui/layout/layout.tsx
+++ frontend/src/components/ui/layout/layout.tsx
@@ -14,14 +14,7 @@ const Layout = ({ children }: PropsWithChildren) => {
             <main className="min-h-screen w-full px-4 py-8">
                 {children}
             </main>
-            <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
-                <div className="container mx-auto px-4 text-center text-gray-600">
-                    <p> Hello testing </p>
-                </div>
-            </footer>
-
         </div>
-
     );
 };
 
